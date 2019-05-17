2b510b48cc144a1ce2979770e6fa2d359ff2a190
michael-raul-arboleda <michael.raul.arboleda@gmail.com>
2019-05-17 19:37:16 -0400

    Created Header and Post-Create components

---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html',
+  styleUrls: ['./post-create.component.css']
+})
+export class PostCreateComponent implements OnInit {
+
+  enteredValue: string;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
