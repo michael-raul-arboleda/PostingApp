a59ae09c810e34b2c28231e1a6884a763313bc1c
michael-raul-arboleda <michael.raul.arboleda@gmail.com>
2019-05-18 23:41:07 -0400

    Defined data as type Post

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,5 @@
 import {Component, Input, OnInit} from '@angular/core';
+import {Post} from '../post.model';
 
 @Component({
   selector: 'app-post-list',
@@ -12,7 +13,7 @@ export class PostListComponent implements OnInit {
   //   {title: 'T3', content: 'c3'}
   // ];
 
-  @Input() posts = [];
+  @Input() posts: Post[] = [];
 
   constructor() { }
 
